078ce2cddad358ce82f0ab78369c93312bd33868
AndreNavarro <andre-navarro@hotmail.com>
2021-10-24 09:37:36 -0300

    :sparkles: Autenticação Github - Login/Logout - #NLW07

--- src/components/SigninBox/index.tsx
+++ src/components/SigninBox/index.tsx
@@ -5,8 +5,10 @@ import { Button } from '../Button';
 import { COLORS } from '../../theme';
 
 import { styles } from './styles';
+import { useAuth } from '../../hooks/auth';
 
 export function SigninBox(){
+  const { signIn, isSigningIn } =  useAuth();
   return (
     <View style={styles.container}>
 
@@ -15,6 +17,8 @@ export function SigninBox(){
         color={COLORS.BLACK_PRIMARY}
         backgroundColor={COLORS.YELLOW}
         icon="github"
+        onPress={signIn}
+        isLoading={isSigningIn}
       />
 
     </View>
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -5,15 +5,17 @@ import { Header } from '../../components/Header';
 import { MessageList } from '../../components/MessageList';
 import { SendMessageForm } from '../../components/SendMessageForm';
 import { SigninBox } from '../../components/SigninBox';
+import { useAuth } from '../../hooks/auth';
 
 import { styles } from './styles';
 
 export function Home() {
+  const { user } = useAuth();
   return (
     <View style={styles.container}>
       <Header />
       <MessageList />
-      <SendMessageForm />
+      { user ? <SendMessageForm /> : <SigninBox />}
     </View>
   )
 }
